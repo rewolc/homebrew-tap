11950ba9c136be71538896ea6edaa88c4cb82a01
Belyaev Egor <95301875+rewolc@users.noreply.github.com>
2026-04-06 00:52:42 +0300

    chore: update sovia to v0.4.2

--- Casks/sovia.rb
+++ Casks/sovia.rb
@@ -2,7 +2,7 @@ cask "sovia" do
   version "0.4.2"
   sha256 "a1c6b602d35484d3936ef4b880e00552f8d95fd78cbd7e71b17f4ce73faede8c"
 
-  url "https://github.com/rewolc/sovia-releases/releases/download/v#{version}/Sovia-mac-arm64.dmg"
+  url "https://github.com/rewolc/sovia-releases/releases/download/v\#{version}/Sovia-mac-arm64.dmg"
   name "Sovia"
   desc "Desktop AI copilot for technical interviews"
   homepage "https://sovia.space"
@@ -11,7 +11,7 @@ cask "sovia" do
 
   postflight do
     system_command "/usr/bin/xattr",
-                   args: ["-dr", "com.apple.quarantine", "#{appdir}/Sovia.app"],
+                   args: ["-dr", "com.apple.quarantine", "\#{appdir}/Sovia.app"],
                    sudo: false
   end
 

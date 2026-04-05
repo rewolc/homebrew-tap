4b4b59e5d6005faf5daa496569b900921d9f1738
Belyaev Egor <95301875+rewolc@users.noreply.github.com>
2026-04-06 02:09:59 +0300

    chore: update sovia to v0.4.3

--- Casks/sovia.rb
+++ Casks/sovia.rb
@@ -2,7 +2,7 @@ cask "sovia" do
   version "0.4.3"
   sha256 "4c26c26534715c5fe9fd71a27ce5245028083951f5a2425c421c2be57c1eca7f"
 
-  url "https://github.com/rewolc/sovia-releases/releases/download/v#{version}/Sovia-mac-arm64.dmg"
+  url "https://github.com/rewolc/sovia-releases/releases/download/v0.4.3/Sovia-mac-arm64.dmg"
   name "Sovia"
   desc "Desktop AI copilot for technical interviews"
   homepage "https://sovia.space"

61d9fb007a4ecf6b71320d058c6701ebff46d315
Belyaev Egor <95301875+rewolc@users.noreply.github.com>
2026-04-06 00:48:15 +0300

    chore: update sovia to v0.4.2

--- Casks/sovia.rb
+++ Casks/sovia.rb
@@ -1,6 +1,6 @@
 cask "sovia" do
-  version "0.4.1"
-  sha256 "4c578977ead036d530d2bc113978fd28b7c7d5aff6df2b9588814e08eaf493ff"
+  version "0.4.2"
+  sha256 "a1c6b602d35484d3936ef4b880e00552f8d95fd78cbd7e71b17f4ce73faede8c"
 
   url "https://github.com/rewolc/sovia-releases/releases/download/v#{version}/Sovia-mac-arm64.dmg"
   name "Sovia"
